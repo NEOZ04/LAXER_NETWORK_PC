97d7252028beed678a33fd581419274044ebb978
NEOZ04 <zulfahmineo@gmail.com>
2020-08-29 16:18:21 +0700

    add command spawn to back to default spawn

--- plugins_dev/Core/src/laxer/Main.java
+++ plugins_dev/Core/src/laxer/Main.java
@@ -1,7 +1,9 @@
 package laxer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,13 +17,27 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
+		
+		switch (command.getName().toLowerCase()) {
+			case "test": 
+				if (sender instanceof Player) {
+					sender.sendMessage("[Sang OP] Eyyyoooo Whats Up " + sender.getName());	
+				}else {
+					sender.sendMessage("[Sang OP] Eyyyoooo Whats Up");	
+				}
+				break;
+			case "spawn": 
+				if (sender instanceof Player) {
+					
+					((Player) sender).teleport(this.getServer().getWorld("world").getSpawnLocation());
+					sender.sendMessage(ChatColor.GREEN + "Teleport to spawn...");
+					return true;
+				}
+				sender.sendMessage(ChatColor.RED + "Please use this command in game.");
+				break;
+		}
+		
 		if (command.getName().equalsIgnoreCase("test")) {
-			if (sender instanceof Player) {
-				sender.sendMessage("[Sang OP] Eyyyoooo Whats Up " + sender.getName());	
-			}else {
-				sender.sendMessage("[Sang OP] Eyyyoooo Whats Up");	
-			}
 		}
 		
 		return false;
